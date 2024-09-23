12b31ddf4f352b911c0d28cc8a0100e36b3b9b1d
Gonzalo Mansilla <gonzalo2nm@gmail.com>
2024-09-22 22:47:34 -0300

    creacion de movie controller pero metodos sin  implementear, y eliminacion de comentarios y espacios en el program.cs

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -4,6 +4,7 @@ using backend_cine.Models;
 using backend_cine.Dbcontext;
 using backend_cine.Interfaces;
 using backend_cine.Responses;
+using Microsoft.VisualBasic;
 
 namespace backend_cine.Controllers;
 
@@ -12,28 +13,63 @@ namespace backend_cine.Controllers;
 [Route("api/movies")]
 public class MovieController : ControllerBase, ICrud<Movie>
 {
-  public Task<ActionResult<ResponseOne<Movie>>> Add(Movie obj)
+  private readonly DbContext _context;
+
+  public MovieController(DbContext dbContext)
   {
-    throw new NotImplementedException();
+    _context = dbContext;
   }
 
-  public Task<ActionResult<ResponseOne<Movie>>> Delete(long id)
-  {
-    throw new NotImplementedException();
-  }
-
+  [HttpGet]
+  [ProducesResponseType(StatusCodes.Status200OK)]
+  [ProducesResponseType(StatusCodes.Status500InternalServerError)]
   public Task<ActionResult<ResponseList<Movie>>> FindAll()
   {
     throw new NotImplementedException();
   }
 
+  [HttpGet("{id}")]
+  [ProducesResponseType(StatusCodes.Status200OK)]
+  [ProducesResponseType(StatusCodes.Status400BadRequest)]
+  [ProducesResponseType(StatusCodes.Status404NotFound)]
+  [ProducesResponseType(StatusCodes.Status500InternalServerError)]
   public Task<ActionResult<ResponseOne<Movie>>> FindOne(long id)
   {
     throw new NotImplementedException();
   }
 
+  [HttpPost]
+  [Consumes("application/json")]
+  [ProducesResponseType(StatusCodes.Status201Created)]
+  [ProducesResponseType(StatusCodes.Status400BadRequest)]
+  [ProducesResponseType(StatusCodes.Status409Conflict)]
+  [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+  public Task<ActionResult<ResponseOne<Movie>>> Add(Movie obj)
+  {
+    throw new NotImplementedException();
+  }
+
+  [HttpPut("{id}")]
+  [Consumes("application/json")]
+  [ProducesResponseType(StatusCodes.Status200OK)]
+  [ProducesResponseType(StatusCodes.Status400BadRequest)]
+  [ProducesResponseType(StatusCodes.Status404NotFound)]
+  [ProducesResponseType(StatusCodes.Status409Conflict)]
+  [ProducesResponseType(StatusCodes.Status500InternalServerError)]
   public Task<ActionResult<ResponseOne<Movie>>> Update(long id, Movie obj)
   {
     throw new NotImplementedException();
   }
+
+  [HttpDelete("{id}")]
+  [ProducesResponseType(StatusCodes.Status200OK)]
+  [ProducesResponseType(StatusCodes.Status400BadRequest)]
+  [ProducesResponseType(StatusCodes.Status404NotFound)]
+  [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+  public Task<ActionResult<ResponseOne<Movie>>> Delete(long id)
+  {
+    throw new NotImplementedException();
+  }
+
+
 }
--- Program.cs
+++ Program.cs
@@ -1,32 +1,23 @@
 using backend_cine.Dbcontext;
 using Microsoft.EntityFrameworkCore;
 
-
 var builder = WebApplication.CreateBuilder(args);
 string? connectionString = builder.Configuration.GetConnectionString("SqlServerConnection");
-
 builder.Services.AddDbContext<DbContextCinema>(options =>
 {
     options.UseSqlServer(connectionString);
     options.EnableSensitiveDataLogging();
 });
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
-
 app.Run();
